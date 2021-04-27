307b3ec8b10caedd89b78b4640453bc265bcc8a4
akash15raj <akash15raj>
2021-04-27 16:36:38 +0530

    Updates

---
+++ precache-manifest.1e00f2bf1cdd4bf9a9c768e1bfc18ab1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbd505da8112680ced96",
+    "url": "/shoppingcart/static/css/main.2ccc7bff.chunk.css"
+  },
+  {
+    "revision": "bbd505da8112680ced96",
+    "url": "/shoppingcart/static/js/main.bbd505da.chunk.js"
+  },
+  {
+    "revision": "877f65fc7abf97360959",
+    "url": "/shoppingcart/static/css/1.d1d6cbb9.chunk.css"
+  },
+  {
+    "revision": "877f65fc7abf97360959",
+    "url": "/shoppingcart/static/js/1.877f65fc.chunk.js"
+  },
+  {
+    "revision": "947db2b407c85a628daa",
+    "url": "/shoppingcart/static/js/runtime~main.947db2b4.js"
+  },
+  {
+    "revision": "5f320abc9b6cafb8eeef1fc8029396b3",
+    "url": "/shoppingcart/static/media/item1.5f320abc.jpeg"
+  },
+  {
+    "revision": "ee8183425fdd717d1b1947b1cc6ea509",
+    "url": "/shoppingcart/static/media/item2.ee818342.jpeg"
+  },
+  {
+    "revision": "ad32d9dd47854d00f045243b81d7eef2",
+    "url": "/shoppingcart/static/media/item3.ad32d9dd.jpeg"
+  },
+  {
+    "revision": "cd7ed88c16cd547ca5ba11e0bde07edd",
+    "url": "/shoppingcart/static/media/item4.cd7ed88c.jpeg"
+  },
+  {
+    "revision": "04f8fcb788d48579f1e0cea7cf0b9e05",
+    "url": "/shoppingcart/static/media/item5.04f8fcb7.jpeg"
+  },
+  {
+    "revision": "0e468f0d31c3e30aaf18591dda671485",
+    "url": "/shoppingcart/static/media/item6.0e468f0d.jpeg"
+  },
+  {
+    "revision": "0423f8ecbd1b943f9095658077f6aeea",
+    "url": "/shoppingcart/index.html"
+  }
+];
